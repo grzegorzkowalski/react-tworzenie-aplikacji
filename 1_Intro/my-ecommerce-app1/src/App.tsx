c8ff39151516e2fbb7e3345d55ae176c3bb73aca
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2025-01-12 17:07:30 +0100

    tailwindcss

---
+++ 1_Intro/my-ecommerce-app1/src/App.tsx
@@ -0,0 +1,14 @@
+import 'virtual:windi.css'
+
+function App() {
+
+  return (
+      <>
+          <div className="text-center text-2xl text-blue-500">
+              Tailwind CSS działa!
+          </div>
+      </>
+  )
+}
+
+export default App
